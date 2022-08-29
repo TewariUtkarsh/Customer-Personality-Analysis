f0722bbbe530e48f4fe4b4fc9cc66d5603053d57
UT <tewariutkarsh@outlook.com>
2022-08-29 10:26:51 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,25 +2,43 @@ import os
 from setuptools import setup, find_packages
 from typing import List
 
+
+
+# Constants required for setup file
 REQUIEMENTS_FILE_PATH = 'requirements.txt'
 PROJECT_NAME = "customer-analysis"
 VERSION = '0.0.1'
 DESC = "This is classification problem on Customer Segmentation"
 AUTHOR_NAME = 'Utkarsh Tewari'
 
-def get_requirments_packages():
-    if os.path.exists(REQUIEMENTS_FILE_PATH):
-        packages=[]
-        with open(REQUIEMENTS_FILE_PATH, 'r') as req:
-            for p in req.readlines():
-                if p!=' ' and p!='' and p!='\n':
-                    packages.append(p.replace('\n',''))
 
-        return packages[:-1]
-    else:
+
+def get_requirments_packages() -> List[str]:
+    """
+    This functions reads the requirements.txt file
+    and returns the list of packages mentioned in it.
+
+    Returns
+    -------
+    list :
+        List of all packages mentioned in requirements.txt
+    """
+    
+    try:
+        if os.path.exists(REQUIEMENTS_FILE_PATH):
+            packages=[]
+            with open(REQUIEMENTS_FILE_PATH, 'r') as req:
+                for p in req.readlines():
+                    if p!=' ' and p!='' and p!='\n':
+                        packages.append(p.replace('\n',''))
+
+            return packages[:-1]
+        else:
+            raise Exception('Requirements file does not exist')
+    except Exception as e:
         print("Requirements file does not exist")
 
-print(get_requirments_packages())
+
 
 setup(
     name=PROJECT_NAME,
