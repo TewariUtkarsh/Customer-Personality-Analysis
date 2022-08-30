64859a2e3239fb9d547e98face9f76cea50fa378
UT <tewariutkarsh@outlook.com>
2022-08-30 12:33:41 +0530

    config entity updated for data_ingestion

---
+++ customer/entity/config_entity.py
@@ -0,0 +1,18 @@
+from collections import namedtuple
+from unicodedata import name
+
+
+# Training Pipeline Config
+TrainingPipelineConfig= namedtuple('TrainingPipelineConfig',
+['training_artifact_dir'])
+
+
+# Data Ingestion Config
+DataIngestionConfig= namedtuple('DataIngestionConfig',
+['download_data_url', 'raw_data_dir', 'ingested_train_dir', 'ingested_test_dir'])
+
+
+# Data Validation Config
+# DataValidationConfig= namedtuple('DataValidationConfig',
+# ['schema_file_path', 'report_file_path', 'report_page_file_path'])
+
