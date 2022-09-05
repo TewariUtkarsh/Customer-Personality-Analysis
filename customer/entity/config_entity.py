8b8a634443854731f3479baad2f50e4f482c87dc
UT <tewariutkarsh@outlook.com>
2022-09-05 22:38:00 +0530

    Updated config entity for data transformation

--- customer/entity/config_entity.py
+++ customer/entity/config_entity.py
@@ -16,3 +16,9 @@ DataIngestionConfig= namedtuple('DataIngestionConfig',
 DataValidationConfig= namedtuple('DataValidationConfig',
 ['schema_file_path', 'report_file_path', 'report_page_file_path'])
 
+
+
+# Data Transformation Config
+DataTransformationConfig= namedtuple('DataTransformationConfig',
+['transformed_train_dir', 'transformed_test_dir', 'preprocessed_model_object_file_path'])
+
