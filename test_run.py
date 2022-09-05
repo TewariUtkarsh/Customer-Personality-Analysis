1235bad40666058adda61d2275f89e140e030d93
UT <tewariutkarsh@outlook.com>
2022-09-05 22:25:07 +0530

    Updated test_run

--- test_run.py
+++ test_run.py
@@ -3,6 +3,7 @@ from flask_cors import CORS, cross_origin
 from customer.pipeline.pipeline import Pipeline
 from customer.exception import CustomerException
 from customer.logger import logging
+from customer.config.configuration import Configuration
 import os, sys
 
 
@@ -12,15 +13,22 @@ app= Flask(__name__)
 @cross_origin()
 def index():
     try:
-        logging.info("Running Pipeline")
-        raise Exception('test')
-        p= Pipeline()
-        res=p.run_pipeline()
-        print(res)
-        logging.info("Pipeline ran successfully")
-        return f"Test Run Successfull [500]"
+        # logging.info("Running Pipeline")
+        # # raise Exception('test')
+        # p= Pipeline()
+        # res=p.run_pipeline()
+        # print(res)
+        # logging.info("Pipeline ran successfully")
+        # return f"Test Run Successfull [500]"
+        c= Configuration()
+        print(c.get_data_validation_config())
     except Exception as e:
         raise CustomerException(e, sys) from e
 
 if __name__=='__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    # c= Configuration()
+    # print(c.get_data_validation_config())
+    p= Pipeline()
+
+    print(p.run_pipeline())
