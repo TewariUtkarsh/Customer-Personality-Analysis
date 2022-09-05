aada10a4edbfc8bdc736b6fe07700d0279e55afb
UT <tewariutkarsh@outlook.com>
2022-09-05 22:44:01 +0530

    Updated pipeline for data validation

--- customer/pipeline/pipeline.py
+++ customer/pipeline/pipeline.py
@@ -3,8 +3,9 @@ from customer.exception import CustomerException
 from customer.logger import logging
 from customer.config.configuration import Configuration
 from customer.components.data_ingestion import DataIngestion
-from customer.entity.config_entity import DataIngestionConfig
-from customer.entity.artifact_entity import DataIngestionArtifact
+from customer.components.data_validation import DataValidation
+from customer.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from customer.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 
 
 class Pipeline:
@@ -12,6 +13,7 @@ class Pipeline:
         try:
             config_object = Configuration()
             self.data_ingestion_config:DataIngestionConfig= config_object.get_data_ingestion_configuration()
+            self.data_validation_config:DataIngestionConfig= config_object.get_data_validation_configuration()
         except Exception as e:
             raise CustomerException(e, sys) from e
 
@@ -24,10 +26,26 @@ class Pipeline:
             raise CustomerException(e, sys) from e
 
     
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        try:
+            data_validation= DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config=self.data_validation_config
+            )
+
+
+            data_validation_artifact= data_validation.initiate_data_validation()
+            return data_validation_artifact
+
+        except Exception as e:
+            raise CustomerException(e, sys) from e
+
+
     def run_pipeline(self):
         try:
             data_ingestion_artifact= self.start_data_ingestion()
-            return data_ingestion_artifact
+            data_validation_artifact= self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            return data_validation_artifact
         except Exception as e:
             raise CustomerException(e, sys) from e
 
