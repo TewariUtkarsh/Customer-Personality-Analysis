ceb1e31be1a2b02db301623bfe3bdc1499b6fc89
UT <tewariutkarsh@outlook.com>
2022-09-06 23:21:21 +0530

    updated utils for data transformation

--- customer/utils/util.py
+++ customer/utils/util.py
@@ -1,9 +1,11 @@
 import os, sys
 import yaml
 import pandas as pd
+import numpy as np
 from customer.exception import CustomerException
 from customer.logger import logging
 import json
+import dill, pickle
 
 
 def read_yaml_file(filepath: str) -> dict:
@@ -40,9 +42,27 @@ def csv_to_df(filepath: str, sep:str=',') -> pd.DataFrame:
         raise CustomerException(e, sys) from e
 
 
-# def save_json_file(obj:dict, filepath: str) -> None:
-#     try:
-#         with open(filepath, 'w') as f:
-#             json.dump(obj, f, indent=6)
-#     except Exception as e:
-#         raise CustomerException(e, sys ) from e
+def save_json_file(obj:dict, filepath: str) -> None:
+    try:
+        with open(filepath, 'w') as f:
+            json.dump(obj, f, indent=6)
+    except Exception as e:
+        raise CustomerException(e, sys ) from e
+
+
+def save_numpy_data(filepath:str, data:np.array) -> None:
+    try:
+        os.makedirs(os.path.dirname(filepath), exist_ok=True)
+        with open(filepath, 'wb') as file_object:
+            np.save(file_object, data)
+    except Exception as e:
+        raise CustomerException(e, sys) from e
+
+
+def save_model_object(filepath:str, model_object) -> None:
+    try:
+        os.makedirs(os.path.dirname(filepath))
+        with open(filepath, 'wb') as file_object:
+            dill.dump(model_object, file_object)
+    except Exception as e:
+        raise CustomerException(e, sys) from e
